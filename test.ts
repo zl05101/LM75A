54cf6feb9e34f59cba635fefec3e8995514f6548
lz05101 <328222051@qq.com>
2020-06-16 11:26:04 +0800

    aa

--- test.ts
+++ test.ts
@@ -1,7 +1,3 @@
-// tests go here; this will not be compiled when this package is used as an extension.
-basic.forever(() => {
-    let temp = LM75A.getTemperatureInDegrees()
-    basic.pause(1000)
-    serial.writeValue("teamp:",10)
-
-})
+let temp = LM75A.getTemperatureInDegrees()
+basic.pause(1000)
+serial.writeValue("teamp:",10)
